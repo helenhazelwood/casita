9420d69155c879f53a31b95f15f2e40811660a29
Helen <helenhisaac@gmail.com>
2019-09-25 17:58:04 -0400

    feat(api): POST new plant routes working and tested

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>STACKATHON</h1>
+    <h1>Casita</h1>
     <nav>
       {isLoggedIn ? (
         <div>
--- server/api/plants.spec.js
+++ server/api/plants.spec.js
@@ -51,6 +51,44 @@ describe('Plant routes', () => {
 
       expect(res.body.name).to.deep.equal(testPlant.name)
       expect(res.body.userId).to.deep.equal(testPlant.userId)
+    }) //end GET single plant
+  }) //end GET routes
+  describe('POST /:userId', () => {
+    let testPlant
+    let testUser
+    let testUser2
+    beforeEach(async () => {
+      testPlant = await Plant.create({
+        name: 'Monstera',
+        sunlight: 'indirect',
+        soil: 'porous',
+        temperature: [65, 90],
+        humidity: 'high'
+      })
+      testUser = await User.create({
+        email: 'gardener@earth.edu',
+        password: 'shmelon'
+      })
+      testUser2 = await User.create({
+        email: 'iloveplants@garden.com',
+        password: 'shmelon'
+      })
+      await testPlant.setUser(testUser)
+    }) //end test seed hook
+    it('creates a new plant instance and associates it with the specified user', async () => {
+      const res = await request(app)
+        .post(`/api/plants/${testUser2.id}`)
+        .send({
+          name: 'Pothos',
+          sunlight: 'indirect',
+          soil: 'dense',
+          temperature: [65, 80],
+          humidity: 'medium'
+        })
+        .expect(201)
+
+      expect(res.body.name).to.deep.equal('Pothos')
+      expect(res.body.user.id).to.deep.equal(testUser2.id)
     })
   })
 })
