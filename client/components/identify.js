995d89dba4b86b0135e26cca0f750ebb9361a971
Helen <helenhisaac@gmail.com>
2019-09-27 15:56:30 -0400

    feat(identify): image preview and base64 encode working

--- client/components/identify.js
+++ client/components/identify.js
@@ -1,76 +1,54 @@
 import React from 'react'
-import IdentifyForm from './identify-form'
-import {identifyNewPlant} from '../store/identify'
-import {connect} from 'react-redux'
-
-const initialState = {
-  user: {},
-  plant: {
-    userId: null,
-    imageDataURL: ''
-  }
-}
 
 class Identify extends React.Component {
   constructor(props) {
     super(props)
-    this.state = initialState
-    this.handleChange = this.handleChange.bind(this)
+    this.state = {file: '', imagePreviewURL: ''}
+    this.handleImageChange = this.handleImageChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
   }
 
   handleSubmit(event) {
     event.preventDefault()
-    this.props.identifyPlant(this.state.imageDataURL, this.state.user.id)
-    this.setState(initialState)
+    let dataURL = this.state.imagePreviewURL.replace(
+      /^data:image\/[a-z]+;base64,/,
+      ''
+    )
   }
-
-  readImage(event) {
-    let input = event.target.value
-
-    let file = input.files[0]
-    const reader = new FileReader()
+  handleImageChange(event) {
+    event.preventDefault()
+    let reader = new FileReader()
+    let file = event.target.files[0]
     reader.onloadend = () => {
-      let b64 = reader.result.replace(/^data:.+;base64,/, '')
-      console.log('b64', b64)
+      this.setState({
+        file: file,
+        imagePreviewURL: reader.result
+      })
     }
-    reader.readAsDataURL(file) //i'm not sure what the point of this is if the b64 dataUrl has already been generated above
+    reader.readAsDataURL(file)
   }
-
   render() {
+    let {imagePreviewURL} = this.state
+    let $imagePreview = null
+    if (imagePreviewURL) {
+      $imagePreview = <img src={imagePreviewURL} />
+    } else {
+      $imagePreview = <p>Please Select An Image for Preview</p>
+    }
+
     return (
-      <IdentifyForm
-        handleChange={this.handleChange}
-        handleSubmit={this.handleSubmit}
-        user={this.state}
-        plant={this.state}
-      />
+      <div className="identify-form">
+        <form>
+          <input
+            className="fileInput"
+            type="file"
+            onChange={this.handleImageChange}
+          />
+        </form>
+        <div className="image-preview">{$imagePreview}</div>
+      </div>
     )
   }
 }
 
-const mapDispatch = dispatch => ({
-  identifyPlant: imageDataURL => dispatch(identifyNewPlant(imageDataURL))
-})
-
-const mapState = state => ({
-  user: state.user
-})
-
-export default connect(mapState, mapDispatch)(Identify)
-
-// export const Identify = (props) => {
-//   const {user} = props
-//   return (
-//     <div className="identify">
-//       <h2>Identify a new plant:</h2>
-//       <IdentifyForm user={user} />
-//     </div>
-//   )
-// }
+export default Identify
